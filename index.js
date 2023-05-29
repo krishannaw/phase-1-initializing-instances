cf516b3682ca80d1bcdc4a65a88a3ee0c20c7659
Krishanna Williams <krishannaw@gmail.com>
2023-05-29 02:10:23 -0500

    bld

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your code here
+class Breakfast {
+    constructor(food, drink) {
+      this.food = food;
+      this.drink = drink;
+    }
+  }
+  
+  class Lunch {
+    constructor(salad, soup, drink) {
+      this.salad = salad;
+      this.soup = soup;
+      this.drink = drink;
+    }
+  }
+  
+  class Dinner {
+    constructor(salad, soup, entree, dessert) {
+      this.salad = salad;
+      this.soup = soup;
+      this.entree = entree;
+      this._dessert = dessert; // Private property for dessert indicated by naming convention
+    }
+  }
+  
+  // Testing the classes
+  const breakfast = new Breakfast("Toast", "Coffee");
+  console.log(breakfast);
+  
+  const lunch = new Lunch("Caesar Salad", "Tomato Soup", "Iced Tea");
+  console.log(lunch);
+  
